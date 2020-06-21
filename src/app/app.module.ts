5365e6e3025fd3b0420ec1a9c43a56c4dc0aefcd
Turbo <adiy@oent.pl>
2020-06-21 15:37:24 +0200

    To do List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DummyComponent } from './dummy/dummy.component';
@@ -14,7 +15,8 @@ import { SomeComponent } from './some/some.component';
     SomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
