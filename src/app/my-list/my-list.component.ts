5985fd75fc6246083ea81b594adca64306276199
Maciej Rapacki <mac.rapacki@gmail.com>
2020-06-21 10:51:36 +0100

    - add button wich add new item to list - add the x button for each element of the list after clicking which the item will be removed from the list

--- src/app/my-list/my-list.component.ts
+++ src/app/my-list/my-list.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-my-list',
   templateUrl: './my-list.component.html',
-  styleUrls: ['./my-list.component.scss']
+  styleUrls: ['./my-list.component.scss'],
 })
 export class MyListComponent implements OnInit {
-  arr = ["lista1", "lista2", "lista3"];
+
+  arr = ["lista1", "lista2", "lista3", "lista4"];
+  newLIstNumber = 4;
+
   @Output() public sendData: EventEmitter<number> = new EventEmitter();
+
 
   constructor() { }
 
@@ -16,6 +20,14 @@ export class MyListComponent implements OnInit {
 
   public sendNumber() {
     this.sendData.emit(this.arr.length);
+  }
+
+  addItem() {
+    this.arr.push(`lista${this.arr.length + 1}`)
+  }
+
+  remove(i) {
+    this.arr.splice(i, 1)
   }
 
 }
--- src/app/some/some.component.ts
+++ src/app/some/some.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SomeComponent implements OnInit {
   num: number = 2;
+
+
   @Input() public data: number;
+  @Input() public listItem: number;
 
   constructor() { }
 
